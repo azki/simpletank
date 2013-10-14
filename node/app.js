b37e7900b0d4b7817f9e9e7f9214cb73fbed30e9
azki <azki@daumcorp.com>
2013-10-14 23:30:17 +0900

    createRoom 과 join 까지 하다가 ..

--- node/app.js
+++ node/app.js
@@ -1,4 +1,4 @@
-/*jslint regexp:false,nomen:false*/
+/*jslint regexp:false,nomen:false,white:false*/
 /*global exports,require,__dirname*/
 
 var OUT_DIR = __dirname + '/../out';
@@ -16,7 +16,8 @@ app.use(express.static(OUT_DIR));
 // '/' 는 모바일 접속 용.
 // '/monitor' 는 방 만들기 용.
 var redirectMonitor = function (req, res) {
-	res.redirect('/monitor.html');
+	var param = req.query.limit ? '?limit=' + req.query.limit : '';
+	res.redirect('/monitor.html' + param);
 };
 app.get('/mon', redirectMonitor);
 app.get('/monitor', redirectMonitor);
@@ -26,53 +27,82 @@ server.listen(89);
 //remotor
 
 var sockMap = {};
-var roomCount = 0;
 var roomArr = [];
+
+function getRoomNumFromMonId(monId) {
+	var roomNum = -1;
+	roomArr.forEach(function (room, index) {
+		if (room.monId === monId) {
+			roomNum = index;
+		}
+	});
+	return roomNum;
+}
 
 io.of('/tank').on('connection', function(socket) {
 	var sockId = socket.id;
 	sockMap[sockId] = {
-		sockId: sockId
+		sockId: sockId,
+		socket: socket
 	};
 	socket.on('disconnect', function() {
 		delete sockMap[sockId];
-		socket.broadcast.to('temp_room').emit('temp_event', {
-			hi: 1,
-			hello: 2
-		});
+		var roomNum = getRoomNumFromMonId(sockId);
+		if (roomNum > -1) {
+			roomArr.splice(roomNum, 1);
+			socket.leave('room' + roomNum);
+			socket.broadcast.to('room' + roomNum).emit('error', '방이 닫혔습니다. (게임 종료)');
+		}
 	});
 	
 	//mon - 방 만들기
-	socket.on('createRoom', function() {
-		roomArr[roomCount] = {
-			mon: sockId
+	socket.on('createRoom', function(roomLimit) {
+		var roomNum = roomArr.length;
+		roomArr[roomNum] = {
+			monId: sockId,
+			joinable: true,
+			user: [],
+			userLimit: roomLimit
 		};
-		socket.emit('newRoom', roomCount);
-		roomCount += 1;
+		socket.join('room' + roomNum);
+		socket.emit('newRoom', roomNum);
 	});
-	//mon - 방 들어오기
-	socket.on('join', function(roomNum) {
-		if (!roomArr[roomNum]) {
-			//TODO return error
-			return;
+	//mon - 누구의 차례인가
+	socket.on('turn', function(tankIndex) {
+		var roomNum = getRoomNumFromMonId(sockId);
+		if (roomNum > -1) {
+			socket.broadcast.to('room' + roomNum).emit('turn', tankIndex);
 		}
-		var monSockId = roomArr[roomNum].mon;
-		if (monSockId in sockMap == false) {
-			//TODO return error
-			return;
-		}
-		socket.emit('okRoom', roomNum);
 	});
-	
 	
 	//mon - 게임시작
-	
-	//mon - 누구의 차례인가 (유저 / 논유저)
-	
 	//mon - 게임 끝
 	
 	
-	
+	//remote - join
+	socket.on('join', function(userInfo) {
+		var roomNum, room;
+		roomNum = userInfo.roomNum;
+		room = roomArr[roomNum];
+		if (!room) {
+			socket.emit('error', '존재하지 않는 방입니다.');
+			return;
+		}
+		if (room.joinable === false) {
+			socket.emit('error', '현재 입장 가능한 상태가 아닙니다. 게임이 끝난 후 입장 가능.');
+			return;
+		}
+		if (room.user.length >= room.userLimit) {
+			socket.emit('error', '현재 방이 꽉 찼습니다.');
+			return;
+		}
+		userInfo.sockId = sockId;
+		room.user.push(userInfo);
+		socket.join('room' + roomNum);
+		sockMap[room.monId].socket.emit('updateRoom', room);
+		//socket.broadcast.to('room' + roomNum).emit('userUpdate', room.user);
+		socket.emit('okRoom', roomNum);
+	});
 	
 });
 
